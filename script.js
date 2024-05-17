a9f9a6c46ef5e9a40f7a6273d0d5b1b5c2b5502c
Aron Barbosa <154107475+aronbarbosag@users.noreply.github.com>
2024-05-17 00:34:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,4 @@ const audioInico = new Audio('xmen_theme.mp3');
 
 audioInico.play();
 audioInico.volume = 0.005;
-window.addEventListener('change', tocarMusica);
-function tocarMusica() {
-  audioInico.play();
-  console.log('mudou');
-}
+
